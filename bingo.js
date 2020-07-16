439d9dd04393fda40072fc3aa670a7edf82d4aa8
Guilherme Mesquita <52768525+guilhermesquita@users.noreply.github.com>
2020-07-15 22:18:48 -0400

    Add files via upload

---
+++ bingo.js
@@ -0,0 +1,104 @@
+//////NUMEROS ALEATORIOS
+const getRandomIntegerInclusive = (min, max) => 
+Math.floor(Math.random() * (max - min+1)) + min
+let ran = getRandomIntegerInclusive(1, 50)
+
+//CELULA ( 1,1 )
+let num = document.getElementById('num')
+num.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 1,2 )
+let num1 = document.getElementById('num1')
+num1.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 1,3 )
+let num2 = document.getElementById('num2')
+num2.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 1,4 )
+let num3 = document.getElementById('num3')
+num3.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 1,4 )
+let num4 = document.getElementById('num4')
+num4.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 2,1 )
+let num5 = document.getElementById('num5')
+num5.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 2,2 )
+let num6 = document.getElementById('num6')
+num6.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 2,3 )
+let num7 = document.getElementById('num7')
+num7.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 2,4 )
+let num8 = document.getElementById('num8')
+num8.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 2,5 )
+let num9 = document.getElementById('num9')
+num9.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 3,1 )
+let num10 = document.getElementById('num10')
+num10.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 3,2 )
+let num11 = document.getElementById('num11')
+num11.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 3,2 )
+//let num12 = document.getElementById('num12')
+//num12.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 3,4 )
+let num13 = document.getElementById('num13')
+num13.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 3,5 ) 
+let num14 = document.getElementById('num14')
+num14.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 4,1 ) 
+let num15 = document.getElementById('num15')
+num15.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 4,2 ) 
+let num16 = document.getElementById('num16')
+num16.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 4,3 ) 
+let num17 = document.getElementById('num17')
+num17.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 4,4 ) 
+let num18 = document.getElementById('num18')
+num18.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 4,5 ) 
+let num19 = document.getElementById('num19')
+num19.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 5,1 ) 
+let num20 = document.getElementById('num20')
+num20.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 5,2 ) 
+let num21 = document.getElementById('num21')
+num21.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 5,3 ) 
+let num22 = document.getElementById('num22')
+num22.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 5,4 ) 
+let num23 = document.getElementById('num23')
+num23.innerHTML = getRandomIntegerInclusive(1, 75)
+
+//CELULA ( 5,5 ) 
+let num24 = document.getElementById('num24')
+num24.innerHTML = getRandomIntegerInclusive(1, 75)
